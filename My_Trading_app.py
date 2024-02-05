2129e097a5c1f987a10f2529e40feb3b4223d8bf
Dalvik <99361351+DalvikLoger@users.noreply.github.com>
2024-02-05 13:12:46 +0100

    My Trading App

---
+++ My_Trading_app.py
@@ -0,0 +1,22 @@
+import streamlit as st
+import pandas as pd
+
+st.set_page_config(
+    page_title="Trading app",
+    page_icon="👋",
+)
+
+st.write("# Welcome to Trading App! 👋")
+
+st.sidebar.success("Select a demo above.")
+
+st.markdown(
+    """
+    Trading App is an open-source app built specifically for
+    Machine Learning and Data Science with time-series projects.
+    **👈 Select a demo from the sidebar** to see some examples
+    of what Tradin App can do!
+    ### Want to learn more?
+    - Explore more project [streamlit.io](https://www.gitlab.com/DalvikLoger)
+""")
+
