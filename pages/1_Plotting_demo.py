c5c9e8a1627ff647e204b83755ffe04bda1c122b
Dalvik <99361351+DalvikLoger@users.noreply.github.com>
2024-02-06 12:15:49 +0100

    My trading Modification 1.6

--- pages/1_Plotting_demo.py
+++ pages/1_Plotting_demo.py
@@ -2,9 +2,6 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-from plotly.offline import init_notebook_mode
-init_notebook_mode(connected=True)
-import plotly.figure_factory as ff
 import statsmodels.api as sm
 from numpy.random import normal, seed
 from scipy.stats import norm
